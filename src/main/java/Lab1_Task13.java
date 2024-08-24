8ee990707446dfd2c327750b32d263d5422b0c09
noturcoderfr <mirzahazimy29@gmail.com>
2024-08-24 11:50:17 +0000

    update again

--- src/main/java/Lab1_Task13.java
+++ src/main/java/Lab1_Task13.java
@@ -1,10 +1,15 @@
 public class Lab1_Task13 {
 	public static void main(String[] args) {
 
-		double x = 10, y =5;
+		double x = 10, y = 5;
 		// Declare and initialise x and y.
 
 		System.out.println(Math.PI*Math.max(4*y,Math.abs(x - y)));
+		// Prints out the result.
+
+		// Math.PI - Gives the value of the constant pi.
+		// Math.max - Gives the greatest between 2 values.
+		// Math.abs - Gives the absolute value.
 		
 	}
 
--- src/main/java/Lab1_Task15.java
+++ src/main/java/Lab1_Task15.java
@@ -19,32 +19,31 @@
 //		- balance2 ( second year balance )
 //		- balance3 ( third year balance )
 
-// Create 3 methods to calculate each year's balance after gaining interest:
+// Create 3 static methods to calculate each year's balance after gaining interest:
 //		- new balance = initial balance + ( initial balance * 0.05 interest )
-//		- Print strings stating the year with its respective new ammount after interest.
+//		- Print strings stating the year with its respective new amount after interest.
 
 
 public class Lab1_Task15 {
 
-	public static double first_year(double iniBalance, double intYear) {
-		double balance1 = iniBalance + (iniBalance * intYear);
-		System.out.println("First year: " + balance1);
-		return balance1; // Return balance1 to be used in second year method.
+	public static double first_year(double iniBalance, double intYear) {  // First static method.
+		double balance1 = iniBalance + (iniBalance * intYear);  // Calculates the balance after interest of the first year.
+		System.out.println("First year: $" + balance1);
+		return balance1;  // Return balance1 to be used in second year method.
 		
 	}
 
-	public static double second_year(double balance1, double intYear) {
-		double balance2 = balance1 + (balance1 * intYear);
-		System.out.println("Second year: " + balance2);
-		return balance2; // Return balance2 to be used in third year method.
+	public static double second_year(double balance1, double intYear) {  // Second static method.
+		double balance2 = balance1 + (balance1 * intYear);  // Calculates the balance after interest of the second year.
+		System.out.println("Second year: $" + balance2);
+		return balance2;  // Return balance2 to be used in third year method.
 		
 	}
 
-	public static double third_year(double balance2, double intYear) {
-		
-		double balance3 = balance2 + (balance2 * intYear);
-		System.out.println("Third year: " + balance3);
-		return balance3; // Return balance3.
+	public static double third_year(double balance2, double intYear) {  // Third static method.
+		double balance3 = balance2 + (balance2 * intYear);  // Calculates the balance after interest of the third year.
+		System.out.println("Third year: $" + balance3);
+		return balance3;  // Return balance3.
 		
 	}
 
@@ -53,9 +52,9 @@ public class Lab1_Task15 {
 		double iniBalance = 1000;
 		double intYear = (double) 5/100;
 
-		double balance1 = first_year(iniBalance, intYear); // Store the returned value
-		double balance2 = second_year(balance1, intYear); // Store the returned value
-		double balance3 = third_year(balance2, intYear); // Store the returned value
+		double balance1 = first_year(iniBalance, intYear);  // Store the returned value
+		double balance2 = second_year(balance1, intYear);  // Store the returned value
+		double balance3 = third_year(balance2, intYear);  // Store the returned value
 		
 	}
 	
