b203030bc0dc03c70a21cb3a869d042a5d2895ee
noturcoderfr <mirzahazimy29@gmail.com>
2024-08-24 10:08:10 +0000

    lab1 task15 steps update

--- src/main/java/Lab1_Task15.java
+++ src/main/java/Lab1_Task15.java
@@ -1,34 +1,62 @@
+// Write a program that prints the balance of an account after the first, second, and third year. The account has an initial balance of $1,000 and earns 5 percent interest per year. 
+
+// (Hint: Calculate first year interest. The first year balance will be the sum of initial balance and first year interest. Then continue to calculate second year balance using first year balance and so on.)
+
+// -----Summary-----
+// An account with initial balance of $1000 earns 5% interest per year:
+//		Outcome ---> $1000 would increase to $1050 after a year.
+
+// Expected output:
+// First year: $1050
+// Second year: $1102.50
+// Third year: $1157.625 / $1157.63
+
+// -----Program-----
+// Declare and initialise variables:
+//		- inBalance ( initial balance )
+//		- ipYear ( interest per year )
+//		- balance1 ( first year balance )
+//		- balance2 ( second year balance )
+//		- balance3 ( third year balance )
+
+// Create 3 methods to calculate each year's balance after gaining interest:
+//		- new balance = initial balance + ( initial balance * 0.05 interest )
+//		- Print strings stating the year with its respective new ammount after interest.
+
+
 public class Lab1_Task15 {
 
 	public static double first_year(double iniBalance, double intYear) {
 		double balance1 = iniBalance + (iniBalance * intYear);
 		System.out.println("First year: " + balance1);
-		return balance1; // Return the calculated balance
+		return balance1; // Return balance1 to be used in second year method.
+		
 	}
 
 	public static double second_year(double balance1, double intYear) {
 		double balance2 = balance1 + (balance1 * intYear);
 		System.out.println("Second year: " + balance2);
-		return balance2; // Return the calculated balance
+		return balance2; // Return balance2 to be used in third year method.
+		
 	}
 
 	public static double third_year(double balance2, double intYear) {
+		
 		double balance3 = balance2 + (balance2 * intYear);
 		System.out.println("Third year: " + balance3);
-		return balance3; // Return the calculated balance
+		return balance3; // Return balance3.
+		
 	}
 
 	public static void main(String[] args) {
-
-		double balance1 = 0;
-		double balance2 = 0;
-		double balance3 = 0;
-
+		
 		double iniBalance = 1000;
 		double intYear = (double) 5/100;
 
-		balance1 = first_year(iniBalance, intYear); // Store the returned value
-		balance2 = second_year(balance1, intYear); // Store the returned value
-		balance3 = third_year(balance2, intYear); // Store the returned value
+		double balance1 = first_year(iniBalance, intYear); // Store the returned value
+		double balance2 = second_year(balance1, intYear); // Store the returned value
+		double balance3 = third_year(balance2, intYear); // Store the returned value
+		
 	}
+	
 }
