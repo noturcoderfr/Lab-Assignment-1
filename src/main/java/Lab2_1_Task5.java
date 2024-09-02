719270b87739849c4ebe070b6d39486a1bac19b0
noturcoderfr <mirzahazimy29@gmail.com>
2024-09-02 23:38:46 +0000

    added lab 2.2 task 4

---
+++ src/main/java/Lab2_1_Task5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lab2_1_Task5 {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Please enter the number of bags: ");
+		String numberOfUnits = input.next();
+
+		System.out.println("Please enter the weight per bag (in Kg): ");
+		String unitWeight = input.next();
+		
+	}
+	
+}
